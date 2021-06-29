0214927be0e5f6ae6b9eba3d07965a2c4eff1b04
keimoriyama <keischwiiz@gmail.com>
2021-06-29 16:33:09 +0900

    add test

--- test/data_test.py
+++ test/data_test.py
@@ -1,11 +1,29 @@
-import unittest
 import sys
+import unittest
+
+import numpy as np
+
 sys.path.append('../src/')
 
-from data import template_dataset
+from data import image_dataset, template_dataset
 
-class Testuilts(unittest.TestCase):
-        def test_dataset(self):
+image_size = 16
+num_image = 5
+labels = 3
+
+data = np.random.rand(image_size, image_size, num_image)
+label = np.random.randint(0, labels, (num_image))
+
+
+class TestDatasets(unittest.TestCase):
+    def test_imagedataset(self):
+        dataset = image_dataset(data, label)
+        print("test of dataset length")
+        self.assertEqual(num_image, len(dataset))
+
+    def Test_errorcase(self):
+        dataset = image_dataset(data)
+
 
 if __name__ == '__main__':
     unittest.main()
